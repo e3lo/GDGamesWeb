f991783d0b9be957855c9e086ca14220231236d1
Leo Heng <leoheng905@gmail.com>
2024-05-27 17:27:40 +1000

    Added item retrieval

--- js/components/categorySection.js
+++ js/components/categorySection.js
@@ -1,16 +1,74 @@
 import { Component } from '../framework/component.js';
+import { getItemById } from '../framework/itemHandler.js';
 
 export default class CategorySection extends Component {
   constructor(document) {
     super('app-category-section', document);
 
     this.props = {
-      body: '',
+      id: '',
+      title: '',
+      description: '',
+      stat: '',
+      statDescription: '',
+      itemDescription: '',
+      itemTitle: '',
+      src1: '',
+      src2: '',
+      src3: '',
+      items: '',
     };
   }
 
   createElement(parentNode) {
     const templateClone = super.createElement(parentNode);
+
+    this.setImage(
+      templateClone.querySelector('.category-section__bg'),
+      `linear-gradient(rgba(48, 48, 48, 0.4), rgba(23, 23, 23, 1)),
+    url(${this.props.src1})`
+    );
+
+    this.setImage(
+      templateClone.querySelector('.category-section__bento__stat'),
+      `linear-gradient(rgba(23, 23, 23, 0.6), rgba(23, 23, 23, 0.6)),
+      url(${this.props.src2})`
+    );
+
+    this.setImage(
+      templateClone.querySelector('.category-section__bento__img'),
+      `url(${this.props.src3})`
+    );
+
+    // Setting the item list
+    const itemSection = templateClone.querySelector('.category-collection');
+    const items = this.props.items.split(',');
+    items.forEach((value) => {
+      const item = getItemById(parseInt(value));
+      if (item == null) {
+        return;
+      }
+
+      const element = document.createElement('app-item');
+      element.setAttribute('id', item.id);
+      element.setAttribute('title', item.title);
+      element.setAttribute('type', item.productOptions[0].title);
+      element.setAttribute('description', item.tags);
+      element.setAttribute('src', item.productImg[0]);
+      itemSection.appendChild(element);
+    });
+
+    // items.appendChild(this.createElement('app-item'));
+
     return templateClone;
+  }
+
+  setImage(node, backgroundStyle) {
+    if (node.style == null) {
+      return;
+    }
+    node.style.background = backgroundStyle;
+    node.style.backgroundSize = 'cover';
+    node.style.backgroundPosition = 'center';
   }
 }
---
+++ js/framework/itemHandler.js
@@ -0,0 +1,5 @@
+import itemList from '../data/itemList.js';
+
+export function getItemById(id) {
+  return itemList[id];
+}
