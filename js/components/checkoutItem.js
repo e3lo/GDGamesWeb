23ab04a7513cd87ba9a68a077cab26892c75ec6a
Leo Heng <leoheng905@gmail.com>
2024-05-29 22:05:23 +1000

    Added remove from cart

--- js/components/checkoutItem.js
+++ js/components/checkoutItem.js
@@ -30,36 +30,52 @@ export default class CheckoutItem extends Component {
     const subtotal = templateClone.querySelector('#subtotal');
 
     let shippingCost = parentNode.getAttribute('shipping') === 'true' ? 10 : 0;
-
-    price.innerText = '$' + parentNode.getAttribute('price');
-    qty.innerText = parentNode.getAttribute('qty');
-    shipping.innerText =
-      parentNode.getAttribute('shipping') === 'true' ? '$10' : 'Free';
-    subtotal.innerText = `$${
+    let subtotalValue =
       shippingCost +
       parseInt(
         parentNode.getAttribute('price') *
           parseInt(parentNode.getAttribute('qty'))
-      )
-    }`;
+      );
+    price.innerText = '$' + parentNode.getAttribute('price');
+    qty.innerText = parentNode.getAttribute('qty');
+    shipping.innerText =
+      parentNode.getAttribute('shipping') === 'true' ? '$10' : 'Free';
+    subtotal.innerText = `$${subtotalValue}`;
+
+    parentNode.setAttribute('subtotal', subtotalValue);
 
     return templateClone;
   }
 
   setQuantity(item, value) {
+    if (value == 0) {
+      if (window.confirm('Do you want to remove this item from your cart?')) {
+        this.removeItem(item);
+      }
+      return;
+    }
+
+    // Updating display
     const quantityDisplay = item.querySelector('#item-quantity__value');
     quantityDisplay.innerHTML = value;
 
+    // Updating attributes
+    const shippingCost = item.getAttribute('shipping') === 'true' ? 10 : 0;
+    const itemCost = parseInt(item.getAttribute('price')) * value;
+    item.setAttribute('subtotal', shippingCost + itemCost);
+    item.querySelector('#subtotal').innerText = `$${shippingCost + itemCost}`;
+
+    // Setting local storage
     let cart = JSON.parse(localStorage.getItem('cart'));
     let itemData = cart[parseInt(item.getAttribute('index'))];
     itemData.quantity = value;
     localStorage.setItem('cart', JSON.stringify(cart));
 
-    item.setAttribute('quantity', value);
+    item.setAttribute('qty', value);
   }
 
   getQuantity(item) {
-    return parseInt(item.getAttribute('quantity'));
+    return parseInt(item.getAttribute('qty'));
   }
 
   // Handling Qty
@@ -87,5 +103,23 @@ export default class CheckoutItem extends Component {
     this.setQuantity(item, this.getQuantity(item) + 1);
     // May require checks for limit here aswell
     console.log(this.getQuantity(item));
+  }
+
+  removeItem(item) {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    const index = item.getAttribute('index');
+
+    cart.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    console.log(localStorage.getItem('cart'));
+
+    // Updating every other nodes index
+    const parent = item.parentNode;
+    item.remove();
+
+    Array.from(parent.children).forEach((value, index) => {
+      value.setAttribute('index', index);
+    });
   }
 }
